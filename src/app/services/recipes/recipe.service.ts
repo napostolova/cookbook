235f3bc182f38f5eb902d021e4f146480864eb07
Nadya Apostolova <napostolova01@gmail.com>
2022-04-29 22:47:52 +0300

    Add edit and delete functionality

--- src/app/services/recipes/recipe.service.ts
+++ src/app/services/recipes/recipe.service.ts
@@ -2,18 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 import { IRecipe } from "../../shared/interfaces/recipe";
+import { UserService } from '../user/user.service';
 
 const apiUrl = 'http://localhost:4000/recipes';
 
 let token: any;
-token = localStorage.getItem('token');
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type':  'application/json',
-    'x-authorization': token
-  })
-}
+
+// const httpOptions = {
+//   headers: new HttpHeaders({
+//     'Content-Type': 'application/json',
+//     'x-authorization': token
+//   })
+// }
 
 
 @Injectable({
@@ -21,20 +22,36 @@ const httpOptions = {
 })
 export class RecipeService {
 
-  constructor(private http: HttpClient) { }
+
+  constructor(private http: HttpClient,
+    public userService: UserService  ) {
+      token= userService.token;
+  }
 
   getRecipes() {
     return this.http.get<IRecipe[]>(`${apiUrl}`);
   }
 
-  getRecipe(id: string){
+  getRecipe(id: string) {
     return this.http.get<IRecipe>(`${apiUrl}/${id}`);
   }
   createRecipe(data: any) {
-    return this.http.post<IRecipe>(`${apiUrl}`, data, httpOptions);
+    return this.http.post<IRecipe>(`${apiUrl}`, data,  {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'x-authorization': token
+      })
+    }
+    );
   }
 
   likeRecipe(id: string) {
-    return this.http.get<IRecipe>(`${apiUrl}/likes/${id}`, httpOptions)
+    return this.http.get<IRecipe>(`${apiUrl}/likes/${id}`, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'x-authorization': token
+      })
+    }
+    )
   }
 }
